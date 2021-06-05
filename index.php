caa94ae108bc0d2003e43d54a26bf5efc7d94327
Anaclet Ahishakiye <a.anaclet920@gmail.com>
2021-06-05 14:39:19 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Donation Page</title>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" integrity="undefined" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="undefined" crossorigin="anonymous"></script>
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<h3 class="text-center">How much do you want to donate?</h3>
+				<form class="form" method="post" action="">
+					
+				</form>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+
+</body>
+</html>
